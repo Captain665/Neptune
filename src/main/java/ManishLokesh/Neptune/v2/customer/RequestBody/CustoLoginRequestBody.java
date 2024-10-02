424d051bcac5d2f845963f2b043c7d6a605794e2
loharmanish <loharmanish9571@gmail.com>
2024-10-02 08:52:45 +0530

    added global validation

--- src/main/java/ManishLokesh/Neptune/v2/customer/RequestBody/CustoLoginRequestBody.java
+++ src/main/java/ManishLokesh/Neptune/v2/customer/RequestBody/CustoLoginRequestBody.java
@@ -1,21 +1,17 @@
 package ManishLokesh.Neptune.v2.customer.RequestBody;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 public class CustoLoginRequestBody {
 
 
-    @NotNull(message = "mobileNumber should not null")
-    @NotBlank(message = "mobileNumber should not blank")
-    @NotEmpty(message = "mobileNumber should not empty")
-    @Size(min = 10,max = 10, message = "The length of mobile number must be 10 numbers.")
+//    @NotNull(message = "mobileNumber should not null")
+    @NotNull(message = "mobile number should be contain 10 digit and should not be null, blank, empty")
+    @Pattern(regexp = "^[0-9]{10}$", message = "mobile number should be contain 10 digit and should not be null, blank, empty")
     private String mobileNumber;
-    @NotNull(message = "password should not null")
-    @NotBlank(message = "password should not blank")
-    @NotEmpty(message = "password should not empty")
+    @NotNull(message = "password should not be null, blank, empty")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "password should not be null, blank, empty and must contain at least one uppercase letter, one lowercase letter, one digit, one special character, and be at least 8 characters long.")
     private String password;
 
 
